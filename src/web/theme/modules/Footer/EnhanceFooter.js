8c1b4c7991538fac1ef29c6708f4a7239a53c5a3
emile <emile31.roch@gmail.com>
2018-04-25 16:46:37 +0200

    Get the categories datas - refs #11888

--- src/web/theme/modules/Footer/EnhanceFooter.js
+++ src/web/theme/modules/Footer/EnhanceFooter.js
@@ -1,8 +1,24 @@
 import { graphql } from "react-apollo";
+import compose from "recompose/compose";
+import withProps from "recompose/withProps";
+
+const CATEGORY_ID = 49;
 
 export default FooterQuery =>
-  graphql(FooterQuery, {
-    props: ({ data }) => ({
-      store: data.store
+  compose(
+    withProps(props => ({
+      categoryId: CATEGORY_ID
+    })),
+    graphql(FooterQuery, {
+      options: props => ({
+        variables: {
+          id: props.categoryId
+        }
+      }),
+      props: ({ data }) =>
+        console.log("data", data) || {
+          loading: data.loading,
+          category: data.category
+        }
     })
-  });
+  );
