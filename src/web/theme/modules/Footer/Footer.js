802fa5a943b8e35e909a20d052f3c3755e59bd6f
emile <emile31.roch@gmail.com>
2018-04-25 16:47:55 +0200

    Refactor the footer - refs #11888

--- src/web/theme/modules/Footer/Footer.js
+++ src/web/theme/modules/Footer/Footer.js
@@ -4,15 +4,42 @@ import StoreDetails from "../StoreDetails";
 import StoreContact from "../StoreContact";
 import EnhanceFooter from "./EnhanceFooter";
 import FooterQuery from "./FooterQuery.gql";
+import LoadingArea from "theme/ui/molecules/LoadingArea";
 import Logo from "theme/ui/atoms/Logo";
 import "./Footer.scss";
+import Mountain from "./Mountains.png";
 
-const Footer = ({ store }) => {
+const Footer = ({ loading, category }) => {
+  console.log(Mountain);
   return (
     <footer className="footer">
+      <img className="mountain" src={Mountain} alt="mountain" />
       <div className="footer__logo">
         <Logo />
-        <span>Don’t just buy a product, buy an experience!</span>
+        {loading ? (
+          <LoadingArea>Loading main category…</LoadingArea>
+        ) : (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: category.description
+            }}
+          />
+        )}
+      </div>
+      <div className="footer__categories">
+        {loading ? (
+          <LoadingArea>Loading subCategories</LoadingArea>
+        ) : (
+          category.children.map(category => (
+            <div className="category-list-title" key={category.id}>
+              {category.name}
+            </div>
+          ))
+        )}
+      </div>
+      <div className="footer__copyright">
+        Copyright 2018 - Blog & Shop - All right reserved - Developped by Blog &
+        Shop
       </div>
     </footer>
   );
