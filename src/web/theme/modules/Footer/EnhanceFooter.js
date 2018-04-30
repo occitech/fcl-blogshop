ccb4053c3763416fb13f8599718189fc40f1e628
emile <emile31.roch@gmail.com>
2018-04-30 09:20:36 +0200

    Remove unecessary props - refs #11888

--- src/web/theme/modules/Footer/EnhanceFooter.js
+++ src/web/theme/modules/Footer/EnhanceFooter.js
@@ -1,18 +1,14 @@
 import { graphql } from "react-apollo";
 import compose from "recompose/compose";
-import withProps from "recompose/withProps";
 
 const CATEGORY_ID = 49;
 
 export default FooterQuery =>
   compose(
-    withProps(props => ({
-      categoryId: CATEGORY_ID
-    })),
     graphql(FooterQuery, {
       options: props => ({
         variables: {
-          id: props.categoryId
+          id: CATEGORY_ID
         }
       }),
       props: ({ data }) => ({
