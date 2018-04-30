09776988a080c304bb41facff8d36d7be6d9d12f
emile <emile31.roch@gmail.com>
2018-04-30 11:52:51 +0200

    Add specification in the productinfo component - refs #11891

--- src/web/theme/modules/ProductSynthesis/ProductInfo/ProductInfo.js
+++ src/web/theme/modules/ProductSynthesis/ProductInfo/ProductInfo.js
@@ -1,7 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { H2 } from "theme/ui/atoms/Typography/Heading";
 import Actions from "../Actions";
 import Price from "theme/ui/atoms/Typography/Price";
+import infos from "./ProductInfo.json";
 import "./ProductInfo.scss";
 
 const ProductInfo = ({ product }) => {
@@ -16,19 +18,28 @@ const ProductInfo = ({ product }) => {
       <div>
         <Actions product={product} />
       </div>
-      <div>
+      <div className="product-info__specification">
         <H2>Products specifications</H2>
-        <span>
+        <span className="product-info__specification__description">
           You can find here the product dimensions, weight and other usefull
           informations.
         </span>
-      </div>
-      <div>
-        <strong>Deck dimensions:</strong>
-        <span>37,5 x 11,25 inches</span>
+        {infos.map(info => (
+          <div className="product-info__specification__attribute">
+            <strong>{info.attribute}</strong>
+            <span> : {info.value}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
+};
+
+ProductInfo.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    prices: PropTypes.object.isRequired
+  })
 };
 
 export default ProductInfo;
