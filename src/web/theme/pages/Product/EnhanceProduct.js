8bd6272279e74009e0b1bcd6838dc61317b47805
emile <emile31.roch@gmail.com>
2018-06-25 17:56:38 +0200

    Add fake data to handle ununderstood graphql non-synchronization - refs #12187

--- src/web/theme/pages/Product/EnhanceProduct.js
+++ src/web/theme/pages/Product/EnhanceProduct.js
@@ -9,6 +9,40 @@ export default ProductQuery =>
     }),
     props: ({ data }) => ({
       loading: data.loading,
-      product: !data.loading && data.product
+      product: !data.loading && {
+        related: [
+          {
+            name: "Gravity Skateboard",
+            imageUrl: "/media/catalog/product/g/r/gravity-category.png",
+            giftMessageAvailable: false,
+            type_id: "simple",
+            sku: "Gravity Skateboard",
+            path: "/product/Gravity Skateboard",
+            priority: 1,
+            changefreq: null,
+            description: null,
+            desc: null,
+            lastmod: "2018-04-26T11:26:40.000Z",
+            meta_title: "Gravity Skateboard",
+            meta_description: "Gravity Skateboard "
+          },
+          {
+            name: "Roadster Backpack",
+            imageUrl: "/media/catalog/product/r/o/roadster-category.png",
+            giftMessageAvailable: false,
+            type_id: "simple",
+            sku: "Roadster Backpack",
+            path: "/roadster-backpack.html",
+            priority: 1,
+            changefreq: null,
+            description: null,
+            desc: null,
+            lastmod: "2018-04-26T11:07:46.000Z",
+            meta_title: "Roadster Backpack",
+            meta_description: "Roadster Backpack "
+          }
+        ],
+        ...data.product
+      }
     })
   });
