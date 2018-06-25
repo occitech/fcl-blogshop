999554bc7c48be6853008206098d35a6241817ce
emile <emile31.roch@gmail.com>
2018-06-25 17:57:32 +0200

    Make productList more generic - refs #12187

--- src/web/theme/pages/Home/Home.js
+++ src/web/theme/pages/Home/Home.js
@@ -4,13 +4,19 @@ import EnhanceHome from "./EnhanceHome";
 import ProductList from "theme/modules/ProductList";
 import LoadingArea from "theme/ui/molecules/LoadingArea";
 import Page from "theme/ui/templates/Page";
+import GridCatchPhrase from "theme/modules/GridCatchPhrase";
 
 const Home = ({ store, category, loading }) => (
   <Page>
     {loading ? (
       <LoadingArea>Loading products…</LoadingArea>
     ) : (
-      <ProductList products={category.layer.products} />
+      <ProductList
+        catchPhrase={
+          <GridCatchPhrase content="<span>Don’t just buy</span><span class='bold'> a product,</span><span> buy</span><span class='primary'> a complete experience!</span>" />
+        }
+        products={category.layer.products}
+      />
     )}
   </Page>
 );
